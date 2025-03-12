6ca0c3499742b3ba517475da3085f0ff31040ec6
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-03-12 11:51:00 +0100

    Mise en place de l'evenement envoi de mail après le paiement de la commande

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,7 +19,7 @@ final class PurchasePaymentSuccessController extends AbstractController
 
     #[IsGranted('ROLE_USER')]
     #[Route('/purchase/terminate/{id}', name: 'app_payment_success')]
-    public function success($id): Response
+    public function success($id, EventDispatcherInterface $dispatcher): Response
     {
         $user = $this->getUser();
         $purchase = $this->purchaseRepository->find($id);
@@ -32,6 +34,9 @@ final class PurchasePaymentSuccessController extends AbstractController
 
         $purchase->setStatus(Purchase::STATUS_PAID);
         $this->em->flush();
+
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         $this->addFlash('success', 'La commande a été payée et confirmée !');
         return $this->redirectToRoute('app_purchase_list');
