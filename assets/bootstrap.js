39cab7c269911daab69a009888f115f5e81866e8
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-07-04 16:37:49 +0200

    Fix bug autocomplete Google et passe vers le controller stimulus

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import AddressAutocompleteController from './controllers/address_autocomplete_controller.js';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+app.register('address-autocomplete', AddressAutocompleteController);
